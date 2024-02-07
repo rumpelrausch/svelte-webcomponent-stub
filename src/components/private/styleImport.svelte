62f08d253697ad7ab8298007cc8d2c0f34163a77
Philipp Mott <Philipp.Mott@giffits.de>
2024-02-07 12:29:29 +0100

    feat: allows loading of js bundle at any time, with or without "defer"

--- src/components/private/styleImport.svelte
+++ src/components/private/styleImport.svelte
@@ -5,20 +5,23 @@
 	Such imported CSS files MUST NOT be built into the component
 	but loaded at run time to allow browser caching.
 -->
-<svelte:options tag="{null}" />
+<svelte:options tag={null} />
 
 <script>
   import { get_current_component } from "svelte/internal";
   import { basePath } from "../../lib/serviceLocation";
   import { onMount } from "svelte";
 
-	const root = get_current_component().$$.root;
-	onMount(() => {
-		let styleSheet = root.styleSheets[0];
-		if(!styleSheet) {
-			root.appendChild(document.createElement('style'));
-			styleSheet = root.styleSheets[0];
-		}
-		styleSheet.insertRule(`@import "${basePath}/picnic.min.css";`);
-	});
+  const root = get_current_component().$$.root;
+
+  onMount(() => {
+    setTimeout(() => {
+      let styleSheet = root.styleSheets[0];
+      if (!styleSheet) {
+        root.appendChild(document.createElement("style"));
+        styleSheet = root.styleSheets[0];
+      }
+      styleSheet.insertRule(`@import "${basePath}/picnic.min.css";`);
+    }, 0);
+  });
 </script>
